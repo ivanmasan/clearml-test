b599ec21e7bd2c6b9a3c616980b83dbffb6a34f3
Ivan Masan <masan@photoneo.com>
2022-11-15 11:01:16 +0100

    adsad

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-import numpy as np
-from matplotlib import pyplot as plt
-from sklearn.linear_model import LinearRegression
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
-
-def main():
-    X = np.random.rand(100, 10)
-    y = X[:, 0] * 5 + X[:, 4] * 2 + np.random.rand(100) * 4
-
-    model = LinearRegression()
-    model.fit(X, y)
-
-    plt.scatter(model.predict(X), y)
-    plt.show()
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(config):
+    print(OmegaConf.to_yaml(config))
 
 
 if __name__ == "__main__":
