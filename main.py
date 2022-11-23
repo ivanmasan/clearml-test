e593c37cecfa20f224f8f32b190fe794dc4570b7
Ivan Masan <masan@photoneo.com>
2022-11-23 09:27:04 +0100

    adsadasd

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main():
     config_file_yaml = task.connect_configuration(
         name="yaml file", configuration='./conf/config.yaml'
     )
+    print(config_file_yaml)
 
     with open(config_file_yaml, 'r') as f:
         print(f.readlines())
@@ -14,6 +15,7 @@ def main():
     python_file = task.connect_configuration(
         name="py file", configuration='./conf/script.py'
     )
+    print(python_file)
 
     with open(python_file, 'r') as f:
         print(f.readlines())
