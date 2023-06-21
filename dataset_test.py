53dd2841f90d432c94ee151f284a93e62ffac3de
ivanmasan <117279876+ivanmasan@users.noreply.github.com>
2023-06-21 10:54:19 +0200

    Update dataset_test.py

--- dataset_test.py
+++ dataset_test.py
@@ -1,7 +1,7 @@
 from clearml import Dataset
 from pathlib import Path
 
-path = Dataset.get(dataset_id='02804c028b4a493e92649d4ea9e4cbda').get_local_copy()
+path = Dataset.get(dataset_id='408c294aa418412596628325cb5caa1a').get_local_copy()
 path = Path(path)
 
 for item in path.iterdir():
