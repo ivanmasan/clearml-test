cb204ca1a48388c2fbf94dd68e6cf2bac87919ea
ivanmasan <117279876+ivanmasan@users.noreply.github.com>
2023-06-27 13:25:46 +0200

    Update dataset_test.py

--- dataset_test.py
+++ dataset_test.py
@@ -1,8 +1,11 @@
 from clearml import Dataset
 from pathlib import Path
+from time import time
 
-path = Dataset.get(dataset_id='408c294aa418412596628325cb5caa1a').get_local_copy()
+t = time()
+path = Dataset.get(dataset_id='d16b8d4a56074ed484d9c39babb0dc88').get_local_copy()
 path = Path(path)
+print(time() - t)
 
 for item in path.iterdir():
   print(item)
